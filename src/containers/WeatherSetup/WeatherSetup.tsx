89d30433c00666a2de9fe2d79ebb980edbdf10be
EngineeringDeveloper <ryan.c.parki@gmail.com>
2023-03-19 08:39:24 +0000

    Prettier display for debug

--- src/containers/WeatherSetup/WeatherSetup.tsx
+++ src/containers/WeatherSetup/WeatherSetup.tsx
@@ -3,65 +3,73 @@ import { getLocation } from "../../services/location.service";
 import { OpenWeatherOneCallResponse } from "../../types/OpenWeather.types";
 import { oneCallWeather } from "../../utils/RetrieveWeather";
 import { defaultKit } from "../../assets/database";
-import { selectKit, simpleWeather, SimpleWeather } from "../../utils/ChooseKit"
+import { selectKit, simpleWeather, SimpleWeather } from "../../utils/ChooseKit";
 import { KitSelection } from "../../types/User.types";
 
 type WeatherDetails = {
-  lon: number;
-  lat: number;
+    lon: number;
+    lat: number;
 };
 export const WeatherSetup = () => {
-  const [weatherInfo, setWeatherInfo] = useState<WeatherDetails>({
-    lon: 0,
-    lat: 0,
-  });
-  const [weatherResponse, setWeatherResponse] =
-    useState<SimpleWeather>();
-  
-  const [kitSelection, setKitSelection] =
-    useState<KitSelection>();
+    const [weatherInfo, setWeatherInfo] = useState<WeatherDetails>({
+        lon: 0,
+        lat: 0,
+    });
+    const [weatherResponse, setWeatherResponse] = useState<SimpleWeather>();
 
-  const [location, setLocation] = useState<{
-    latitude: number;
-    longitude: number;
-  } | null>(null);
+    const [kitSelection, setKitSelection] = useState<KitSelection>();
 
-  useEffect(() => {
-    const getLocationAndSetState = async () => {
-      const userLocation = await getLocation();
-      setLocation(userLocation);
-    };
-    getLocationAndSetState();
-  }, []);
+    const [location, setLocation] = useState<{
+        latitude: number;
+        longitude: number;
+    } | null>(null);
 
-  useEffect(() => {
-    if (location) {
-      const getWeatherAndSetState = async () => {
-        const weatherData = await oneCallWeather({
-          lat: location.latitude,
-          lon: location.longitude,
-        });
-        setWeatherResponse(simpleWeather(weatherData));
-      };
-      getWeatherAndSetState();
-    }
-  }, [location]);
+    useEffect(() => {
+        const getLocationAndSetState = async () => {
+            const userLocation = await getLocation();
+            setLocation(userLocation);
+        };
+        getLocationAndSetState();
+    }, []);
 
-  return (
-    <div className="App">
-      <div>
-        <h1>Weather Input: {JSON.stringify(weatherResponse)}</h1>
-        <button onClick={() => {
-          if (weatherResponse !== undefined) {
-            selectKit(weatherResponse, defaultKit).then((value) => {
-              setKitSelection(value)
-            }
-            )
-          }
-        }}
-        >Choose Kit</button>
-        <div>Kit Selection: {JSON.stringify(kitSelection)}</div>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        if (location) {
+            const getWeatherAndSetState = async () => {
+                const weatherData = await oneCallWeather({
+                    lat: location.latitude,
+                    lon: location.longitude,
+                });
+                setWeatherResponse(simpleWeather(weatherData));
+            };
+            getWeatherAndSetState();
+        }
+    }, [location]);
+
+    return (
+        <div className='App'>
+            <div>
+          <h1>Weather Input:</h1>
+          <div><pre>{JSON.stringify(weatherResponse, null, "\t")}</pre></div>
+                <button
+                    onClick={() => {
+                        if (weatherResponse !== undefined) {
+                            selectKit(weatherResponse, defaultKit).then(
+                                (value) => {
+                                    setKitSelection(value);
+                                }
+                            );
+                        }
+                    }}
+                >
+                    Choose Kit
+                </button>
+                <div>
+                    <pre>
+                        Kit Selection:{" "}
+                        {JSON.stringify(kitSelection, null, "\t")}
+                    </pre>
+                </div>
+            </div>
+        </div>
+    );
 };
