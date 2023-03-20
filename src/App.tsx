bfa3a09e9bf7a7b2e27b0cfab754abd1c3c3303e
Tom Ford <tomford@Toms-MacBook-Pro.local>
2023-03-20 21:47:19 +0000

    added basic route protection and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import "./App.css";
 import { FirebaseAuth } from "./components/FirebaseAuth";
 import { WeatherSetup } from "./containers/WeatherSetup/WeatherSetup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import AuthRoute from "./components/AuthRoute";
 function App() {
+  console.log("Firebase auth state...", getAuth());
+
   return (
-    <div className="App">
-      <WeatherSetup />
-      <FirebaseAuth />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AuthRoute>
+              <WeatherSetup />
+            </AuthRoute>
+          }
+        />
+        <Route path="/" element={<WeatherSetup />} />
+        <Route path="/login" element={<FirebaseAuth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
