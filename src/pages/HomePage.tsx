3e54922671159b7e1ce22d202560832ed860830a
tommichaelford <38949824+tommichaelford@users.noreply.github.com>
2023-03-24 10:25:10 +0000

    feat: creating a default kit and storing in firestore and linking to user and showing their default kit (#14)
    Co-authored-by: Tom Ford <tomford@Toms-MacBook-Pro.local>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,8 @@
-import { Auth } from "firebase/auth";
+import { Auth, onAuthStateChanged } from "firebase/auth";
 import { Kit } from "../components/Kit";
 
 export const HomePage = (props: { auth: Auth }) => {
   const { auth } = props;
-  console.log("Auth...", auth);
   return (
     <div>
       Homepage.
---
+++ src/types/User.types.ts
@@ -0,0 +1,43 @@
+// Is this overcomplicating it? depends on format of DB
+export type kitOptions = {
+  // kit: {
+  hat: LayerOptions;
+  torso: LayerOptions;
+  legs: LayerOptions;
+  feet: LayerOptions;
+  hands: LayerOptions;
+  // };
+};
+
+export type LayerOptions = {
+  baseLayer: Kit[];
+  outerLayer: Kit[];
+  overLayer: Kit[];
+};
+
+export type LayerSelection = {
+  baseLayer: Kit | null;
+  outerLayer: Kit;
+  overLayer: Kit | null;
+};
+
+//** Return type of main kit selection function */
+export type KitSelection = {
+  hat: LayerSelection;
+  torso: LayerSelection;
+  legs: LayerSelection;
+  feet: LayerSelection;
+  hands: LayerSelection;
+};
+
+//** User owns kit */
+export type Kit = {
+  label: string;
+  id: number | null;
+  tempMin: number;
+  tempMax: number;
+  waterResistance?: boolean;
+  // layerPos?: LayerPos;
+  // layerType: LayerType;
+  // adjustments: FeedBack[];
+};
